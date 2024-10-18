533ce37e4489851dc19405a1fb676ea6bfa00990
Ethan Lerner <yaffoman@gmail.com>
2024-10-17 21:37:57 -0700

    got the map in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import data from '$lib/data.json';
+    import { onMount } from 'svelte';
+
     const images = import.meta.glob('/src/lib/images/*.jpeg', { eager: true });
     let imageList = {};
-
+    let mapObject = null;
     // Load the images and use their filenames as keys
     for (const path in images) {
         const fileName = path.split('/').pop().split('.')[0]; // Get the file name without extension
@@ -17,11 +19,67 @@
         selectedCourse = course;
         selectedHole = null;
         holes = Object.keys(courses[selectedCourse].holes)
+        flyToCourse(course);
     }
     console.log(imageList)
     function selectHole(hole) {
         selectedHole = hole;
     }
+    onMount(async () => {
+        const { Map3DElement } = await google.maps.importLibrary("maps3d");
+        const map = new Map3DElement({
+            center: { lat: 37.36353, lng: -121.9286, altitude: 0 },
+            tilt: 67.5,
+            range: 1000
+        });
+        mapObject = map;
+        document.getElementById('map_container')?.append(map);
+        // 32.697162, -117.207596
+
+        map.flyCameraTo({
+            endCamera: {
+                center: { lat: 32.697162, lng: -117.207596, altitude: 30 },
+                tilt: 67.5,
+                range: 1000
+            },
+            durationMillis: 5000
+        });
+    })
+    function flyToCourse(course) {
+        const courseData = courses[course];
+        mapObject.flyCameraTo({
+            endCamera: {
+                center: { lat: courseData.latitude, lng: courseData.longitude, altitude: 30 },
+                tilt: 67.5,
+                range: 1000
+            },
+            durationMillis: 1000
+        });
+
+    //     For each hole, add a marker
+        for (const hole in courseData.holes) {
+            const holeData = courseData.holes[hole];
+            mapObject.addMarker({
+                position: { lat: holeData.latitude[0], lng: holeData.longitude[0], altitude: 30 },
+                title: `Hole ${hole}`,
+                description: `Par ${holeData.par}`,
+                icon: 'https://maps.google.com/mapfiles/kml/paddle/blu-circle.png'
+            });
+        }
+    }
+    function flyToHole(hole_number, step) {
+        const hole = courses[selectedCourse].holes[selectedHole];
+        mapObject.flyCameraTo({
+            endCamera: {
+                center: { lat: hole.latitude[step], lng: hole.longitude[step], altitude: 30 },
+                tilt: 67.5,
+                range: 1000
+            },
+            durationMillis: 1000
+        });
+    }
+
+
 </script>
 
 <div class="min-h-screen bg-base-200 p-10">
@@ -76,4 +134,5 @@
             </button>
         </div>
     {/if}
+    <div id="map_container" class="h-96"></div>
 </div>

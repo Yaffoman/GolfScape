c534bcdd65a55ed330171c16c096a2c57caa5d4b
Gabriel Beal <38961372+gmbeal@users.noreply.github.com>
2024-10-29 11:18:17 -0700

    course overview close to done

--- src/lib/components/CourseOverview.svelte
+++ src/lib/components/CourseOverview.svelte
@@ -7,14 +7,38 @@
   class="hero min-h-screen"
   style="background-image: url('/src/lib/images/{course.image}');">
   <div class="hero-overlay bg-opacity-60"></div>
-  <div class="hero-content text-neutral-content text-center">
-    <div class="max-w-lg">
-      <h1 class="mb-5 text-5xl font-bold">{course.name}</h1>
-      <p class="mb-5">
-        {course.description}
-      </p>
-      <!-- Stat list -->
-      <button class="btn btn-primary">Get Started</button>
+  <div class="flex flex-col lg:flex-row">
+    <div class="hero-content text-neutral-content text-center lg:text-left">
+        <div class="max-w-lg">
+          <h1 class="mb-5 text-5xl font-bold">{course.name}</h1>
+          <p class="mb-5">
+            {course.description}
+          </p>
+          <!-- Stat list -->
+        </div>
+      </div>
+      <div class="w-11/12 lg:w-auto mx-auto">
+        <div class="stats stats-horizontal lg:stats-vertical shadow bg-opacity-65 bg-accent text-neutral-content w-full mx-auto">
+            <div class="stat">
+              <div class="stat-title text-base-100">Length</div>
+              <div class="stat-value">{course.yardage.black}</div>
+              <div class="stat-desc text-base-100">Black Tees</div>
+            </div>
+          
+            <div class="stat">
+              <div class="stat-title text-base-100">Par</div>
+              <div class="stat-value">{course.par}</div>
+              <!-- <div class="stat-desc">↗︎ 400 (22%)</div> -->
+            </div>
+          
+            <div class="stat">
+              <div class="stat-title text-base-100">Cost</div>
+              <div class="stat-value">{course.cost}</div>
+              <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
+            </div>
+          </div>
     </div>
+      
   </div>
+  
 </div>
